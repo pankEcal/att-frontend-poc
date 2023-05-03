9c90de09290f23ee48722fc865bab96a4f410011
Aswini1902 <aswini.s@enginecacl.com>
2023-05-03 20:53:37 +0530

    Changed Header and HeaderLinks file class component into function component

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -1,46 +1,47 @@
-import React from 'react';
-import { Search } from 'material-ui-icons';
+import React, { useState } from "react";
+import { Search } from "material-ui-icons";
 
-import { withStyles } from 'material-ui';
+import { withStyles } from "material-ui";
 
-import { CustomInput, IconButton as SearchButton } from 'components';
+import { CustomInput, IconButton as SearchButton } from "components";
 
-import { headerLinksStyle } from 'variables/styles';
+import { headerLinksStyle } from "variables/styles";
 
+function HeaderLinks(props) {
+  const [open, setOpen] = useState(false);
 
-class HeaderLinks extends React.Component{
-    state = {
-      open: false,
-    };
-    handleClick = () => {
-      this.setState({ open: !this.state.open });
-    };
+  const handleClick = () => {
+    setOpen({ open: !open });
+  };
 
-    handleClose = () => {
-      this.setState({ open: false });
-    };
-    render(){
-        const { classes } = this.props;
-        
-        return (
-            <div>
-                <CustomInput 
-                    formControlProps={{
-                        className: classes.top + " " + classes.search
-                    }}
-                    inputProps={{
-                        placeholder:"Search",
-                        inputProps:{
-                            'aria-label': 'Search',
-                        }
-                    }}/>
-                <SearchButton color="white" aria-label="edit" customClass={classes.top + " " + classes.searchButton}>
-                    <Search className={classes.searchIcon}/>
-                </SearchButton>           
-                
-            </div>
-        );
-    }
+  const handleClose = () => {
+    setOpen({ open: false });
+  };
+
+  const { classes } = props;
+
+  return (
+    <div>
+      <CustomInput
+        formControlProps={{
+          className: classes.top + " " + classes.search,
+        }}
+        inputProps={{
+          placeholder: "Search",
+          inputProps: {
+            "aria-label": "Search",
+          },
+        }}
+      />
+      <SearchButton
+        color="white"
+        aria-label="edit"
+        customClass={classes.top + " " + classes.searchButton}
+      >
+        <Search className={classes.searchIcon} />
+      </SearchButton>
+    </div>
+  );
 }
 
 export default withStyles(headerLinksStyle)(HeaderLinks);
